23e20166f4d8067616dcbd5c4c59f9f9717aa5c5
Nafis <nafis.ali0123@gmail.com>
2019-04-26 13:23:43 +1000

    Initial Commit
    App foundation

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const config = require("./config/application");
+const database = require("./config/database");
+
+const server = express();
+
+// Setup JWT Key
+server.set("secretKey", config.JWT_SECRET);
+
+// Setup body parser
+server.use(bodyParser.json());
+
+// Default route
+server.get("/", (request, response) => {
+  response.json({ tutorial: "this is a test" });
+});
+
+// Server startup
+server.listen(config.PORT, () => {
+  console.log(`Server started on port: ${config.PORT}`);
+});
+
+// Database connection
+database.connection.on("error", error => console.log(error));
+database.connection.once("open", () => {
+//   server.use("/user", userRoutes);
+//   server.get("/overview", verifyToken, (request, response) => {
+//     response.json({ status: "Success", message: "Route accessed with authorization", data: null });
+//   });
+  console.log("Database connected");
+});
