c0da0cb11f509fe9cb59c0e478977f7706b1d50a
Tushar <tushar.chawla92@gmail.com>
2019-04-30 19:24:21 +1000

    Feature Complete: User
    Added Controlllers and Routes. Modified server.js to use proper routes and controllers

---
+++ server/routes/user.routes.js
@@ -0,0 +1,9 @@
+    
+const userController = require("../controllers/user.controller");
+const express = require("express");
+const router = express.Router();
+
+router.post("/register", userController.create);
+router.post("/authenticate", userController.authenticate);
+
+module.exports = router;
