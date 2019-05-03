b8978e4891a711b0ceff224cac153cc668887ae6
Michael Muni <mmun6233@uni.sydney.edu.au>
2019-05-03 12:02:53 +1000

    Implemented two controller methods, one to find the count of all records in the Revisions model, and another to find the count of all records matching a certain title

--- server/controllers/revision.controller.js
+++ server/controllers/revision.controller.js
@@ -2,19 +2,41 @@ const revisionModel = require("../model/Revision");
 const validator = require("../services/validator");
 
 module.exports = {
-    // Create a new user at registration
-    fetchAll: async (request, response, next) => {
+    // fetch a count of all records
+    countAll: async (request, response, next) => {
 
-        await revisionModel.findOne({minor: false}, function (err, result) { 
+        await revisionModel.count({}, function (err, result) { 
+            //log error to json response if one occurs
             if (err) { 
                 response.json({ status: "error", message: "Could not retrieve results", data: null });
 
                 next(); 
+            //log results to json response if successful
             } else { 
-                response.json({ status: "success", message: "Fetched results", data: result });
+                response.json({ status: "success", message: "Fetched count", data: result });
 
                 next(); 
             }
         });
+    }, 
+
+    //fetch a count of records which match the given title
+    countTitle: async (request, response, next) => {
+        reqTitle = request.query.title; 
+
+        await revisionModel.count({title: reqTitle}, function (err, result) { 
+            //log error to json response if one occurs
+            if (err) { 
+                response.json({ status: "error", message: "Could not retrieve results", data: null });
+
+                next(); 
+            //log results to json response if successful
+            } else { 
+                response.json({ status: "success", message: "Fetched count", data: result });
+
+                next(); 
+            }
+        });
+        
     }
 };
