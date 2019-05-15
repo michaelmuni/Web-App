ed81cd61ca676a6a5ebcf82db77864f641147676
Nafis Ali <nali3710@uni.sydney.edu.au>
2019-05-16 03:13:29 +1000

    Created Article component

---
+++ client/components/Article.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card class="ma-2">
+    <v-card-title primary-title>
+      <div>
+        <h3 class="headline mb-0">{{ title }}</h3>
+        <div>{{ info }}</div>
+      </div>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["title", "info"]
+};
+</script>
+
+<style>
+</style>
