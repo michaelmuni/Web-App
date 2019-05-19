42dd03d029abf127a2c7b0d811140b50ffa280e6
Nafis Ali <nali3710@uni.sydney.edu.au>
2019-05-19 10:11:21 +1000

    Implemented article search
    Implemented article search functionality.
    Selecting article only shows title for now

---
+++ client/components/ArticleContainer.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-card>
+    <v-card-title primary-title>
+      <h3>Title: {{ articleTitle ? articleTitle._id : "N/A" }}</h3>
+    </v-card-title>
+    <v-card-text>Revisions {{ revisionCount }}</v-card-text>
+    <v-divider></v-divider>
+    <v-card-text>
+      Top 5 Regular Users
+      <v-divider></v-divider>
+      <template v-for="item in regUsers">
+        <p :key="item._id">{{ item._id }}</p>
+        <p :key="item._id">Revisions: {{ item.revisions }}</p>
+      </template>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    revisionCount: 0,
+    regUsers: []
+  }),
+  props: ["articleTitle", "yearFrom", "yearTo"],
+  methods: {
+    async getArticleRevisionsInYearRange() {
+      // Get data from server
+      // const data = await this.$axios.$get("revisions/ROUTE_NAME", {
+      //   headers: {
+      //     "x-access-token": this.$store.state.user.authUser.data.token
+      //   },
+      //   params: {
+      //     title: this.articleTitle,
+      // MORE PARAMS CAN BE ADDED
+      //   }
+      // });
+      // Manipulate data and assign to places
+    }
+  },
+  watch: {
+    // Watcher to track article selection and update accordingly
+    // articleTitle: function(value) {
+    //   if(value !== null) {
+    //     this.getArticleInformation();
+    //   }
+    // }
+  }
+};
+</script>
+
+<style>
+</style>
--- client/pages/article.vue
+++ client/pages/article.vue
@@ -1,9 +1,68 @@
 <template>
-  <h1>PLACEHOLDER</h1>
+  <v-layout row>
+    <v-flex class="mr-5 mt-2" xs1>
+      <h4>PLACEHOLDER FOR YEAR INPUT</h4>
+      <v-text-field v-model="counter" :disabled="loading" label="Number" outline></v-text-field>
+    </v-flex>
+    <v-container>
+      <v-toolbar dark color="primary">
+        <v-toolbar-title class="mr-4">
+          <v-icon>search</v-icon>
+        </v-toolbar-title>
+        <v-autocomplete class="mt-2" v-model="title" :hint="'Type to search'" :items="titles" :label="'Search...'" item-text flat solo-inverted>
+          <template v-slot:selection="titles">
+            <v-list-tile-content>
+              <v-list-tile-title v-html="titles.item._id"></v-list-tile-title>
+              <v-list-tile-sub-title v-html="'Revisions: ' + titles.item.revisions"></v-list-tile-sub-title>
+            </v-list-tile-content>
+          </template>
+          <template v-slot:item="titles">
+            <v-list-tile-content>
+              <v-list-tile-title v-html="titles.item._id"></v-list-tile-title>
+              <v-list-tile-sub-title v-html="'Revisions: ' + titles.item.revisions"></v-list-tile-sub-title>
+            </v-list-tile-content>
+          </template>
+        </v-autocomplete>
+      </v-toolbar>
+      <ArticleContainer :articleTitle="title"/>
+    </v-container>
+  </v-layout>
 </template>
 
 <script>
-export default {};
+import ArticleContainer from "~/components/ArticleContainer.vue";
+
+export default {
+  middleware: ["auth"],
+  components: {
+    ArticleContainer
+  },
+  data: () => ({
+    titles: [],
+    title: null,
+    counter: 0,
+    loading: false
+  }),
+  methods: {
+    async getAllTitles() {
+      const data = await this.$axios.$get("revisions/uniqueTitles", {
+        headers: {
+          "x-access-token": this.$store.state.user.authUser.data.token
+        }
+      });
+
+      if (data) {
+        this.titles = data.data;
+      }
+    }
+  },
+  beforeCreate() {
+    this.$store.dispatch("user/initializeStore");
+  },
+  created() {
+    this.getAllTitles();
+  }
+};
 </script>
 
 <style>
